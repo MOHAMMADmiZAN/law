18b16651d300d3242ecc8912de337c78fc48e4d0
MOHAMMAD MIZAN <mohammad25524143@gmail.com>
2020-08-18 08:15:37 +0600

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,246 @@
+$('.banner-main').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+
+    arrows: true,
+    prevArrow: '<i class="fas fa-angle-double-left prev"></i>',
+    nextArrow: '<i class="fas fa-angle-double-right next"></i>',
+    responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false,
+                arrows: true,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false,
+                autoplay: true,
+                autoplaySpeed: 3000,
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false,
+                autoplay: true,
+                autoplaySpeed: 3000,
+
+            }
+        }
+
+    ]
+
+});
+///////////////////////////////wel-part/////////////////////////////////////////////////////
+$('.wel-slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    arrows: true,
+    prevArrow: '<i class="fas fa-angle-double-left prev"></i>',
+    nextArrow: '<i class="fas fa-angle-double-right next"></i>',
+    centerMode:true,
+    centerPadding: '0',
+    responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false,
+                arrows: true,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false,
+                autoplay: true,
+                autoplaySpeed: 3000,
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false,
+                autoplay: true,
+                autoplaySpeed: 3000,
+
+            }
+        }
+
+    ]
+
+});
+//////////////////////////////////////////////////////////////////practice-part/////////////////////////////////////////////////////////////////////////////////////
+$('.pra-slide').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    arrows: true,
+    prevArrow: '<i class="fas fa-angle-double-left prev"></i>',
+    nextArrow: '<i class="fas fa-angle-double-right next"></i>',
+    centerMode:true,
+    centerPadding: '0',
+    responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false,
+                arrows: true,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false,
+                autoplay: true,
+                autoplaySpeed: 3000,
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false,
+                autoplay: true,
+                autoplaySpeed: 3000,
+
+            }
+        }
+
+    ]
+
+});
+//////////////////////////////////////////////////////////testimonial////////////////////////////////////////////////////////////////////////////////////////////////////
+$('.test-slide').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    arrows: true,
+    prevArrow: '<i class="fas fa-angle-double-left prev"></i>',
+    nextArrow: '<i class="fas fa-angle-double-right next"></i>',
+    centerMode:true,
+    centerPadding: '0',
+    responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false,
+                arrows: true,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false,
+                autoplay: true,
+                autoplaySpeed: 3000,
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false,
+                autoplay: true,
+                autoplaySpeed: 3000,
+
+            }
+        }
+
+    ]
+
+});
+
+
+/////////////////////////////////////////////lawyer-part/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+$('.lawyer-tab').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: false,
+    arrows: true,
+    prevArrow: '<i class="fas fa-angle-double-left prev"></i>',
+    nextArrow: '<i class="fas fa-angle-double-right next"></i>',
+    centerMode:true,
+    centerPadding: '0',
+    responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false,
+                arrows: true,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false,
+                autoplay: true,
+                autoplaySpeed: 3000,
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false,
+                autoplay: true,
+                autoplaySpeed: 3000,
+
+            }
+        }
+
+    ]
+
+});
